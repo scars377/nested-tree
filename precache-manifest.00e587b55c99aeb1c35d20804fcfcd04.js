86fd4cfb7255f460801fde9f1605cbb9b86e9507
scars yao <scars377@gmail.com>
2019-03-18 18:47:09 +0800

    Updates

---
+++ precache-manifest.00e587b55c99aeb1c35d20804fcfcd04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "34eca6cb470d43a83922",
+    "url": "./static/js/main.4db02d46.chunk.js"
+  },
+  {
+    "revision": "0502954d5ffb5ab525c6",
+    "url": "./static/js/2.8fe2696f.chunk.js"
+  },
+  {
+    "revision": "34eca6cb470d43a83922",
+    "url": "./static/css/main.588877e3.chunk.css"
+  },
+  {
+    "revision": "20f4c2aa833905944236f14c3503abc4",
+    "url": "./index.html"
+  }
+];
